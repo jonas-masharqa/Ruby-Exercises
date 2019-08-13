060bcd0eed0682283ec0a60dd4642609156b7925
jonas <jonas.masharqa@gmail.com>
2019-08-13 17:28:21 +0200

    Starting over with challenge from scratch, new files

--- my_group.rb
+++ my_group.rb
@@ -1,14 +1,9 @@
-2.3.4 :002 > person = {name: "Mario", age: 30, gender: "male"}
- => {:name=>"Mario", :age=>30, :gender=>"male"} 
-2.3.4 :003 > person = {name: "Luigi", age: 28, gender: "male"}
- => {:name=>"Luigi", :age=>28, :gender=>"male"} 
-2.3.4 :004 > person = {name: "Peach", age: 28, gender: "female"}
- => {:name=>"Peach", :age=>28, :gender=>"female"} 
-2.3.4 :005 > my_group << "Mario"
- => ["Mario"] 
-2.3.4 :006 > my_group << "Luigi"
- => ["Mario", "Luigi"] 
-2.3.4 :007 > my_group << "Peach"
- => ["Mario", "Luigi", "Peach"] 
-2.3.4 :008 > 
+my_group = []
+person = {name: "Person_1", age: 30, gender: "man"}
+person = {name: "Person_2", age: 25, gender: "man"}
+person = {name: "Person_3", age: 30, gender: "woman"}
+my_group << "Person_1"
+my_group << "Person_2"
+my_group << "Person_3"
+
 
