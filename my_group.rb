d9484d8d361aa6ee4864e59c73571ab126fe37e5
jonas <jonas.masharqa@gmail.com>
2019-08-13 16:04:21 +0200

    Starting ruby exercises

---
+++ my_group.rb
@@ -0,0 +1,14 @@
+2.3.4 :002 > person = {name: "Mario", age: 30, gender: "male"}
+ => {:name=>"Mario", :age=>30, :gender=>"male"} 
+2.3.4 :003 > person = {name: "Luigi", age: 28, gender: "male"}
+ => {:name=>"Luigi", :age=>28, :gender=>"male"} 
+2.3.4 :004 > person = {name: "Peach", age: 28, gender: "female"}
+ => {:name=>"Peach", :age=>28, :gender=>"female"} 
+2.3.4 :005 > my_group << "Mario"
+ => ["Mario"] 
+2.3.4 :006 > my_group << "Luigi"
+ => ["Mario", "Luigi"] 
+2.3.4 :007 > my_group << "Peach"
+ => ["Mario", "Luigi", "Peach"] 
+2.3.4 :008 > 
+
