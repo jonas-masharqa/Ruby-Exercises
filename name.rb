30b14aec985b7accce5927c84c92121f5ad672e6
jonas <jonas.masharqa@gmail.com>
2019-08-16 15:24:56 +0200

    Commiting before file changes

--- name.rb
+++ name.rb
@@ -1,3 +1,24 @@
 def hello(name)
     "Hello"+" "+ name +""+"! How are you today?" 
 end
+
+
+
+def hello(name)
+    "Hello"+" "+ name +""+"! How are you today?" 
+    10.times { puts name}
+end
+
+
+
+def hello(name)
+    "Hello"+" "+ name +""+"! How are you today?" 
+end
+
+
+def print "Please type your first name."
+
+user_input = gets
+
+puts "Your first name is #(user_input)!" 
+end
